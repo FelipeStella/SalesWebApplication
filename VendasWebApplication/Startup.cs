2002d0f8f247c61ff9a0860b1ad2df5baad29ec6
Felipe da Silva Stella <luis.felipe@unidasul.com.br>
2022-04-25 17:59:37 -0300

    Adicionado serviço para popular dados no banco de dados em tempo de desenvolvimento.

--- VendasWebApplication/Startup.cs
+++ VendasWebApplication/Startup.cs
@@ -36,16 +36,19 @@ namespace SalesWebApplication
 
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        services.AddDbContext<SalesWebApplicationContext>(options =>
+      services.AddDbContext<SalesWebApplicationContext>(options =>
                 options.UseMySql(Configuration.GetConnectionString("SalesWebApplicationContext"), builder => builder.MigrationsAssembly("SalesWebApplication")));
+
+      services.AddScoped<SeedingService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        seedingService.Seed();
       }
       else
       {
